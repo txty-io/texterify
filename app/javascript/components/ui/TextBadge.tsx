02c6c857c856acef7345d2d2497e7747fec7d822
Christoph Werner <christoph@chrztoph.com>
2019-07-22 18:19:40 +0200

    Adapt text badge design

--- app/javascript/components/ui/TextBadge.tsx
+++ app/javascript/components/ui/TextBadge.tsx
@@ -1,15 +1,16 @@
 import * as React from "react";
+import { Styles } from "./Styles";
 import { TextCopier } from "./TextCopier";
 
 function TextBadge(props: { text: string; withCopy?: string }) {
     return (
         <span
             style={{
-                background: "#f0f0f0",
-                padding: "4px 12px",
-                borderRadius: 80,
+                background: Styles.COLOR_PRIMARY_LIGHT,
+                padding: "4px 8px",
+                borderRadius: 4,
                 fontSize: 11,
-                fontWeight: "bold"
+                color: Styles.COLOR_SECONDARY
             }}
         >
             {props.text}
