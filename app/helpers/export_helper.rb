75d32856d881bed212701e440e69e38a37b8b20b
Christoph Werner <christoph@chrztoph.com>
2019-07-29 11:08:56 +0200

    Add helper to export html translations

--- app/helpers/export_helper.rb
+++ app/helpers/export_helper.rb
@@ -3,6 +3,37 @@ require 'nokogiri'
 require 'yaml'
 
 module ExportHelper
+  def convert_html_translation(content)
+    json_content = JSON.parse(content)
+
+    converted = ''
+    json_content['blocks'].map do |block|
+      if block['type'] == 'list'
+        if block['data']['style'] == 'ordered'
+          converted += '<ol>'
+        elsif block['data']['style'] == 'unordered'
+          converted += '<ul>'
+        end
+
+        block['data']['items'].map do | item |
+            converted += "<li>#{item}</li>"
+        end
+
+        if block['data']['style'] == 'ordered'
+          converted += '</ol>'
+        elsif block['data']['style'] == 'unordered'
+          converted += '</ul>'
+        end
+      elsif block['type'] == 'paragraph'
+        converted += "<p>#{block['data']['text']}</p>"
+      end
+    end
+
+    converted
+  rescue JSON::ParserError
+    ''
+  end
+
   def json(language, export_data)
     language_file = Tempfile.new(language.id.to_s)
     language_file.puts(JSON.pretty_generate(export_data))
