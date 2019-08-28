0b01fe2e00f325312d6a36a976c169f67942718c
Christoph Werner <christoph@chrztoph.com>
2019-08-28 04:55:02 +0200

    Reformat code

--- app/javascript/components/ui/KeyHistory.tsx
+++ app/javascript/components/ui/KeyHistory.tsx
@@ -83,7 +83,10 @@ class KeyHistory extends React.Component<IProps, IState> {
             }
 
             return (
-                <div style={{ display: "flex", flexDirection: "column" }} key={activity.id}>
+                <div
+                    style={{ display: "flex", flexDirection: "column" }}
+                    key={activity.id}
+                >
                     {showDivider && <Divider style={{ fontSize: 14 }}>{date}</Divider>}
                     <div style={{ padding: 8 }}>
                         <div style={{ display: "flex", alignItems: "center", marginBottom: 8 }}>
