ede70af1afd295fb0d84aef7bab65ecdaa6afc88
Christoph Werner <christoph@chrztoph.com>
2019-10-04 17:13:54 +0200

    Change default dev port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost:3010" }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
