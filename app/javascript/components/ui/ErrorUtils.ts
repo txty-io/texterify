112aa1de93aa64e41986b2438388d50848d482d2
Christoph Werner <christoph@chrztoph.com>
2020-05-03 04:13:24 +0200

    Add new error utils

---
+++ app/javascript/components/ui/ErrorUtils.ts
@@ -0,0 +1,48 @@
+import { message } from "antd";
+
+export const ERROR_MESSAGES = {
+    invalid: "{name} is invalid.",
+    taken: "{name} is already in use.",
+    not_found: "{name} could not be found.",
+    blank: "{name} cannot be blank."
+};
+
+type ERRORS_MESSAGE_IDS = keyof typeof ERROR_MESSAGES;
+
+export const ERRORS: { [key: string]: ERRORS_MESSAGE_IDS } = {
+    INVALID: "invalid",
+    TAKEN: "taken",
+    NOT_FOUND: "not_found",
+    BLANK: "blank"
+};
+
+export interface IError {
+    error: ERRORS_MESSAGE_IDS;
+}
+
+export interface IErrorsResponse {
+    [field: string]: IError[];
+}
+
+export const ErrorUtils = {
+    hasError(field: string, error: ERRORS_MESSAGE_IDS, errors: IErrorsResponse) {
+        const fieldErrors = errors[field];
+        return fieldErrors?.some((fieldError) => {
+            return fieldError.error === error;
+        });
+    },
+
+    showErrors(errors: IErrorsResponse) {
+        const keys = Object.keys(errors);
+
+        keys.forEach((key) => {
+            errors[key].forEach((error) => {
+                message.error(this.getErrorMessage(key, error.error));
+            });
+        });
+    },
+
+    getErrorMessage(name: string, error: ERRORS_MESSAGE_IDS) {
+        return ERROR_MESSAGES[error].replace("{name}", name.charAt(0).toUpperCase() + name.slice(1));
+    }
+};
