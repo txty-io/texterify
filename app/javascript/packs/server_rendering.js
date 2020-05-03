65a0c2f03e219d6951c3854ef41bffa6ec8eabb3
Christoph Werner <christoph@chrztoph.com>
2020-05-03 04:16:02 +0200

    Disable linter error

--- app/javascript/packs/server_rendering.js
+++ app/javascript/packs/server_rendering.js
@@ -1,5 +1,6 @@
 // By default, this pack is loaded for server-side rendering.
 // It must expose react_ujs as `ReactRailsUJS` and prepare a require context.
 const componentRequireContext = require.context("components", true);
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
