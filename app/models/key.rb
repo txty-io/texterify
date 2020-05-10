128687877a7d5a13b61c0e98e777ba054390b608
Christoph Werner <christoph@chrztoph.com>
2020-05-11 00:46:25 +0200

    Refactor duplicate keys error

--- app/models/key.rb
+++ app/models/key.rb
@@ -5,20 +5,19 @@ class Key < ApplicationRecord
   has_many :translations, dependent: :destroy
 
   validates :name, presence: true
-  validate :no_duplicate_key_for_project
+  validate :no_duplicate_keys_for_project
 
   before_validation :strip_leading_and_trailing_whitespace
 
-  # Validates that there are no keys with same name
-  # for a project.
-  def no_duplicate_key_for_project
+  # Validates that there are no keys with same name for a project.
+  def no_duplicate_keys_for_project
     project = Project.find(project_id)
     key = project.keys.find_by(name: name)
 
     if key.present?
       updating_key = key.id == id
 
-      errors.add(:name, 'Name is already in use.') if !updating_key
+      errors.add(:name, :taken) if !updating_key
     end
   end
 
