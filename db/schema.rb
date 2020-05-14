cb10c8f457c54ee9fd6b57ab7c14debb110ea145
Christoph Werner <christoph@chrztoph.com>
2020-05-14 22:28:17 +0200

    Renew schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_122855) do
+ActiveRecord::Schema.define(version: 2020_05_14_194008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -87,7 +87,7 @@ ActiveRecord::Schema.define(version: 2020_04_25_122855) do
   end
 
   create_table "languages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.citext "name"
+    t.string "name", null: false
     t.uuid "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
