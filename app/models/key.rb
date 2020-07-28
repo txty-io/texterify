aad5c72adac83b176f59e934bd4656e790386683
Christoph Werner <christoph@chrztoph.com>
2020-07-28 13:37:51 +0200

    Sanitize sql like

--- app/models/key.rb
+++ app/models/key.rb
@@ -2,6 +2,13 @@ class Key < ApplicationRecord
   has_paper_trail
 
   scope :order_by_name, -> { order(arel_table['name'].lower.asc) }
+  scope :ilike_name_or_description_or_translation_content, lambda { |search|
+    where(
+      'keys.name ilike :search or keys.description ilike :search or translations.content ilike :search',
+      search: "%#{sanitize_sql_like(search)}%"
+    )
+  }
+  scope :distinct_on_lower_name, -> { select('distinct on (lower("keys"."name")) keys.*') }
 
   belongs_to :project
   has_many :translations, dependent: :destroy
