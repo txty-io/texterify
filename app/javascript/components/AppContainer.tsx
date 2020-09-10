fbef543d8de27d24372bda1fa020fa74525f5220
Christoph Werner <christoph@chrztoph.com>
2020-09-11 00:54:33 +0200

    Add dark mode favicon

--- app/javascript/components/AppContainer.tsx
+++ app/javascript/components/AppContainer.tsx
@@ -4,7 +4,41 @@ import "../../assets/stylesheets/externals/antd.less";
 import { AppRouter } from "./routing/AppRouter";
 import "mobx-react-lite/batchingForReactDom";
 
+function setFavicon(type: "dark" | "light") {
+    const darkModeIconLink = document.querySelector<HTMLLinkElement>('link[rel="dark-mode-icon"]');
+    const lightModeIconLink = document.querySelector<HTMLLinkElement>('link[rel="light-mode-icon"]');
+    const realIconLink = document.querySelector<HTMLLinkElement>('link[rel="shortcut icon"]');
+
+    if (type === "dark") {
+        realIconLink.href = darkModeIconLink.href;
+    } else if (type === "light") {
+        realIconLink.href = lightModeIconLink.href;
+    }
+}
+
 class AppContainer extends React.Component {
+    componentDidMount() {
+        if (!window.matchMedia) {
+            return;
+        }
+
+        const dark = window.matchMedia("(prefers-color-scheme: dark)");
+
+        if (dark.matches) {
+            setFavicon("dark");
+        } else {
+            setFavicon("light");
+        }
+
+        dark.addEventListener("change", (e) => {
+            if (e.matches) {
+                setFavicon("dark");
+            } else {
+                setFavicon("light");
+            }
+        });
+    }
+
     render() {
         return <AppRouter />;
     }
