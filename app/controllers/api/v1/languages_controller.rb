6caf926325b42a771dc0e5228dbfd10fd34f0d9e
Christoph Werner <christoph@chrztoph.com>
2020-09-28 13:40:04 +0200

    Fix handling of default language

--- app/controllers/api/v1/languages_controller.rb
+++ app/controllers/api/v1/languages_controller.rb
@@ -55,9 +55,11 @@ class Api::V1::LanguagesController < Api::V1::ApiController
     language.language_code = language_code if language_code
     authorize language
 
-    if params[:is_default]
-      current_default_language = project.languages.find_by(is_default: true)
-      current_default_language&.update(is_default: false)
+    if params[:is_default].present?
+      if params[:is_default]
+        current_default_language = project.languages.find_by(is_default: true)
+        current_default_language&.update(is_default: false)
+      end
       language.is_default = params[:is_default]
     end
 
@@ -105,15 +107,17 @@ class Api::V1::LanguagesController < Api::V1::ApiController
     language_code = LanguageCode.find_by(id: params[:language_code])
     language.language_code = language_code if language_code
 
-    if params[:is_default]
-      current_default_language = project.languages.find_by(is_default: true)
-      # Avoid multiple updates of same language.
-      if current_default_language&.id != language.id
-        current_default_language&.update(is_default: false)
+    if params[:is_default].present?
+      if params[:is_default]
+        current_default_language = project.languages.find_by(is_default: true)
+        # Avoid multiple updates of same language.
+        if current_default_language&.id != language.id
+          current_default_language&.update(is_default: false)
+        end
       end
-    end
 
-    language.is_default = params[:is_default]
+      language.is_default = params[:is_default]
+    end
 
     if params.key? :parent
       if params[:parent].present?
