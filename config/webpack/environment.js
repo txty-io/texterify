79ec72cc710b4b448c4b0123ff09162467e6a233
Christoph Werner <christoph@chrztoph.com>
2020-10-07 18:38:51 +0200

    Add frontend env variables

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,14 @@
-const { environment } = require('@rails/webpacker')
-const less = require('./loaders/less')
+const { environment } = require("@rails/webpacker");
+const webpack = require("webpack");
 
-environment.loaders.append('less', less)
-module.exports = environment
+const dotenv = require("dotenv");
+const dotenvFiles = [".env-frontend.development"];
+dotenvFiles.forEach((dotenvFile) => {
+    dotenv.config({ path: dotenvFile, silent: true });
+});
+environment.plugins.insert("Environment", new webpack.EnvironmentPlugin(process.env));
+
+const less = require("./loaders/less");
+environment.loaders.append("less", less);
+
+module.exports = environment;
