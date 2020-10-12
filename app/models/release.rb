6fe7f718f4a3dd950ea11657d0e6259c1807ed38
Christoph Werner <christoph@chrztoph.com>
2020-10-12 17:06:58 +0200

    Add missing validations

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,4 +1,8 @@
 class Release < ApplicationRecord
   belongs_to :export_config
   delegate :project, to: :export_config
+
+  validates :from_version, presence: true
+  validates :to_version, presence: true
+  validates :url, presence: true
 end
