bfbea09aa23ca8961e22f66d8a32c77c339c21ba
Christoph Werner <christoph@chrztoph.com>
2020-10-13 15:04:46 +0200

    Add tests for & in android exports

--- spec/models/export_config_spec.rb
+++ spec/models/export_config_spec.rb
@@ -102,4 +102,28 @@ RSpec.describe ExportConfig, type: :model do
     file.open
     expect(file.read).to eq("<?xml version=\"1.0\"?>\n<resources>\n  <string name=\"x\">\\@</string>\n</resources>\n")
   end
+
+  it 'escapes a & for android' do
+    export_config = ExportConfig.new
+    export_config.file_format = 'android'
+    file = export_config.file(@language, { "x": '&' })
+    file.open
+    expect(file.read).to eq("<?xml version=\"1.0\"?>\n<resources>\n  <string name=\"x\">&amp;</string>\n</resources>\n")
+  end
+
+  it 'escapes two && for android' do
+    export_config = ExportConfig.new
+    export_config.file_format = 'android'
+    file = export_config.file(@language, { "x": '&&' })
+    file.open
+    expect(file.read).to eq("<?xml version=\"1.0\"?>\n<resources>\n  <string name=\"x\">&amp;&amp;</string>\n</resources>\n")
+  end
+
+  it 'does not escape already escaped & for android' do
+    export_config = ExportConfig.new
+    export_config.file_format = 'android'
+    file = export_config.file(@language, { "x": '&&amp;' })
+    file.open
+    expect(file.read).to eq("<?xml version=\"1.0\"?>\n<resources>\n  <string name=\"x\">&amp;&amp;</string>\n</resources>\n")
+  end
 end
