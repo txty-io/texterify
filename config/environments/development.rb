db7f1f0488e9ca7af0e5f29c29a374dae8e7e09e
Christoph Werner <christoph@chrztoph.com>
2020-11-17 03:04:01 +0100

    Improve email settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,11 +51,17 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: "localhost:3000" }
+
+  # Set host so asset_path returns a full URL instead of a relative path.
+  # Otherwise images in emails don't work.
+  config.action_controller.asset_host = 'http://localhost:3000'
+  config.action_mailer.asset_host = config.action_controller.asset_host
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
