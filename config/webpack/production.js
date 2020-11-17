0a53f4c25ff1789add85f9093fddc2f6ba7185a4
Christoph Werner <christoph@chrztoph.com>
2020-11-17 20:49:15 +0100

    Try to tix travis

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -5,7 +5,8 @@ const environment = require("./environment");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 
-console.error("HELP", process.env.COMMIT_HASH);
+console.error("HELP COMMIT_HASH", process.env.COMMIT_HASH);
+console.error("HELP TRAVIS_COMMIT", process.env.TRAVIS_COMMIT);
 
 environment.plugins.append(
     "sentry",
