56a3751d651a4286a7a0e3b798d206abea20c4ea
Christoph Werner <christoph@chrztoph.com>
2020-12-26 05:08:18 +0100

    Fix sidebar trigger

--- app/javascript/components/ui/SidebarTrigger.tsx
+++ app/javascript/components/ui/SidebarTrigger.tsx
@@ -3,5 +3,8 @@ import styled from "styled-components";
 export const SidebarTrigger = styled.div`
     width: 100%;
     height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     transition: 0.2s all;
 `;
