74c17c376376de008543338a3e8e5e707e7046e8
Christoph Werner <christoph@chrztoph.com>
2020-12-28 17:35:27 +0100

    Remove unused ssr pack

--- app/javascript/packs/server_rendering.js
+++
@@ -1,6 +0,0 @@
-// By default, this pack is loaded for server-side rendering.
-// It must expose react_ujs as `ReactRailsUJS` and prepare a require context.
-const componentRequireContext = require.context("components", true);
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
