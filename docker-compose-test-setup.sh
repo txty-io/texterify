04f172bf7fad2b92832f1bd8488a7d33ba724164
Christoph Werner <christoph@chrztoph.com>
2021-02-08 00:50:11 +0100

    Add test setup script

---
+++ docker-compose-test-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+bundle exec rails db:create db:migrate db:seed RAILS_ENV=test
+bundle install --with test
+yarn install --production=false
+apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
