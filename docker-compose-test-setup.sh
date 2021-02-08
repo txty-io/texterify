ce5a78a713c6bd268a7898752078c658740a7e3c
Christoph Werner <christoph@chrztoph.com>
2021-02-08 13:47:35 +0100

    Try setup script

--- docker-compose-test-setup.sh
+++ docker-compose-test-setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -e
 echo "Executing docker-compose-test-setup.sh..."
 
-bundle exec rails db:create db:migrate db:seed RAILS_ENV=test
 bundle install --with test
 yarn install --production=false
 apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
