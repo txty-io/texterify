779be205eca69b679fe3e06ee04576222cea2c4d
Christoph Werner <christoph@chrztoph.com>
2021-05-21 16:59:12 +0200

    Fix database consistency issues

--- app/models/machine_translation_memory.rb
+++ app/models/machine_translation_memory.rb
@@ -1,4 +1,6 @@
 class MachineTranslationMemory < ApplicationRecord
   validates :from, presence: true
   validates :to, presence: true
+  validates :source_language_code_id, presence: true
+  validates :target_language_code_id, presence: true
 end
