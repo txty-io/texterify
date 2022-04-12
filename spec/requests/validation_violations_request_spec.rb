ec18e0f284524d7553914c21f866e6830c7519e3
Christoph Werner <christoph@chrztoph.com>
2022-04-12 12:15:24 +0200

    Fix linter issues

--- spec/requests/validation_violations_request_spec.rb
+++ spec/requests/validation_violations_request_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "ValidationViolations", type: :request do
-
-  describe "GET /index" do
-    it "returns http success" do
-      get "/validation_violations/index"
+RSpec.describe 'ValidationViolations', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/validation_violations/index'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
--- spec/routing/validation_violations_routing_spec.rb
+++ spec/routing/validation_violations_routing_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'ValidationViolationsController', type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: "/validation_violations/index").to route_to("validation_violations#index")
+      expect(get: '/validation_violations/index').to route_to('validation_violations#index')
     end
   end
 end
