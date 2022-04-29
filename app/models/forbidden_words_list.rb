18e709a53a3eaad84439f9f508531f80a4a8a0a8
Christoph Werner <christoph@chrztoph.com>
2022-04-29 02:40:40 +0200

    Delete removed forbidden words that are no longer available

--- app/models/forbidden_words_list.rb
+++ app/models/forbidden_words_list.rb
@@ -12,11 +12,16 @@ class ForbiddenWordsList < ApplicationRecord
   private
 
   def create_forbidden_words
+    found_word_ids = []
+
     splitted = self.content&.split("\n") || []
     splitted.each do |word|
       if word.present?
-        ForbiddenWord.find_or_create_by!(content: word, forbidden_words_list_id: self.id)
+        forbidden_word = ForbiddenWord.find_or_create_by!(content: word, forbidden_words_list_id: self.id)
+        found_word_ids << forbidden_word.id
       end
     end
+
+    self.forbidden_words.where.not(id: found_word_ids).destroy_all
   end
 end
