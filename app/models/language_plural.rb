cc412a5d5455d0930868eed4167d28ad2eaeb077
Christoph Werner <christoph@chrztoph.com>
2022-08-15 02:07:15 +0200

    Add missing presence checker

--- app/models/language_plural.rb
+++ app/models/language_plural.rb
@@ -1,3 +1,8 @@
 class LanguagePlural < ApplicationRecord
   validates :code, presence: true
+  validates :supports_plural_zero, inclusion: [true, false]
+  validates :supports_plural_one, inclusion: [true, false]
+  validates :supports_plural_two, inclusion: [true, false]
+  validates :supports_plural_few, inclusion: [true, false]
+  validates :supports_plural_many, inclusion: [true, false]
 end
