cc1473dc116d18fff86182ed039272829d62f7a4
Christoph Werner <christoph@chrztoph.com>
2022-08-21 01:43:46 +0200

    Add tests

--- spec/models/forbidden_word_spec.rb
+++ spec/models/forbidden_word_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ForbiddenWord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'creates a forbidden word' do
+    forbidden_words_list = ForbiddenWordsList.new
+    forbidden_words_list.name = 'my list'
+    forbidden_words_list.save!
+
+    forbidden_word = ForbiddenWord.new
+    forbidden_word.content = 'bad word'
+    forbidden_word.forbidden_words_list_id = forbidden_words_list.id
+    forbidden_word.save!
+  end
 end
