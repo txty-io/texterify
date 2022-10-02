2bc3c222853ac34ef062e90236a7b7c0f08aa333
Christoph Werner <christoph@chrztoph.com>
2022-10-02 02:47:25 +0200

    Add possibility for different site content wrappers

--- app/javascript/components/ui/LayoutWithSidebarContentWrapperInner.tsx
+++ app/javascript/components/ui/LayoutWithSidebarContentWrapperInner.tsx
@@ -1,9 +1,21 @@
 import { Layout } from "antd";
 import * as React from "react";
 
-export function LayoutWithSidebarContentWrapperInner(props: { children: React.ReactNode }) {
+type ISize = "small" | "medium" | "big";
+
+function getSize(size?: ISize) {
+    if (size === "small") {
+        return 640;
+    } else if (size === "medium") {
+        return 800;
+    } else {
+        return 1200;
+    }
+}
+
+export function LayoutWithSidebarContentWrapperInner(props: { children: React.ReactNode; size?: ISize }) {
     return (
-        <Layout.Content style={{ minHeight: 360, margin: "24px 16px 0px", maxWidth: 1200 }}>
+        <Layout.Content style={{ minHeight: 360, margin: "24px 16px 0px", maxWidth: getSize(props.size) }}>
             {props.children}
         </Layout.Content>
     );
