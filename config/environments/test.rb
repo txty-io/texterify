524a6feadd24499e08658c553a03b92c57ffdb5e
Christoph Werner <christoph@chrztoph.com>
2023-01-03 19:53:17 +0100

    Try to fix CI issue

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,5 +46,8 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
-  config.active_job.queue_adapter = :test
+  config.active_job.queue_adapter = :sidekiq
 end
+
+require 'sidekiq/testing'
+Sidekiq::Testing.inline!
