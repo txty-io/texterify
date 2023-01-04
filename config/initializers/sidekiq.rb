9f52527eeec65df0f8d57157743fd870690583b1
Christoph Werner <christoph@chrztoph.com>
2023-01-04 20:55:20 +0100

    Fix sidekiq setup

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,9 +2,11 @@
 # Learn more here:
 #   - https://github.com/philostler/rspec-sidekiq
 #   - https://github.com/mperham/sidekiq/wiki/Testing
-require 'sidekiq/testing'
-Sidekiq::Testing.inline!
-puts '[sidekiq]: Sidekiq::Testing.inline! called'
+if Rails.env.test?
+  require 'sidekiq/testing'
+  Sidekiq::Testing.inline!
+  puts '[sidekiq]: Sidekiq::Testing.inline! called'
+end
 
 Sidekiq.configure_server { |config| config.redis = { url: 'redis://redis:6379/0' } }
 
