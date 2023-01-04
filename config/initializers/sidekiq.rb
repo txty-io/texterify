b8203f69abb5fde0f779eecbe92b63668a3b68d9
Christoph Werner <christoph@chrztoph.com>
2023-01-04 01:33:24 +0100

    Test something

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,9 +2,9 @@
 # Learn more here:
 #   - https://github.com/philostler/rspec-sidekiq
 #   - https://github.com/mperham/sidekiq/wiki/Testing
-require 'sidekiq/testing'
+# require 'sidekiq/testing'
 Sidekiq::Testing.inline!
-puts '[Sidekiq]: Sidekiq::Testing.inline! called'
+puts '[sidekiq]: Sidekiq::Testing.inline! called'
 
 Sidekiq.configure_server { |config| config.redis = { url: 'redis://redis:6379/0' } }
 
