ab859bcee76283a4d8327dbecea0fc82c5aac9b3
Christoph Werner <christoph@chrztoph.com>
2023-01-14 15:51:54 +0100

    Update cypress default app commands

--- spec/cypress/app_commands/clean.rb
+++ spec/cypress/app_commands/clean.rb
@@ -1,4 +1,22 @@
-DatabaseCleaner.strategy = :truncation
-DatabaseCleaner.clean
+if defined?(DatabaseCleaner)
+  # cleaning the database using database_cleaner
+  DatabaseCleaner.strategy = :truncation
+  DatabaseCleaner.clean
+else
+  logger.warn 'add database_cleaner or update cypress/app_commands/clean.rb'
+  # if defined?(Post)
+  #   Post.delete_all
+  # end
+end
+
+CypressOnRails::SmartFactoryWrapper.reload
+
+if defined?(VCR)
+  VCR.eject_cassette # make sure we no cassette inserted before the next test starts
+  VCR.turn_off!
+  if defined?(WebMock)
+    WebMock.disable!
+  end
+end
 
 Rails.logger.info 'APPCLEANED' # used by log_fail.rb
--- spec/cypress/app_commands/factory_bot.rb
+++ spec/cypress/app_commands/factory_bot.rb
@@ -1,12 +1,16 @@
-Array.wrap(command_options).map do |factory_options|
-  factory_method = factory_options.shift
-  begin
-    logger.debug "running #{factory_method}, #{factory_options}"
-    CypressOnRails::SmartFactoryWrapper.public_send(factory_method, *factory_options)
-  rescue StandardError => e
-    logger.error "#{e.class}: #{e.message}"
-    logger.error e.backtrace.join("\n")
-    logger.error e.record.inspect.to_s if e.is_a?(ActiveRecord::RecordInvalid)
-    raise e
+Array
+  .wrap(command_options)
+  .map do |factory_options|
+    factory_method = factory_options.shift
+    begin
+      logger.debug "running #{factory_method}, #{factory_options}"
+      CypressOnRails::SmartFactoryWrapper.public_send(factory_method, *factory_options)
+    rescue => e
+      logger.error "#{e.class}: #{e.message}"
+      logger.error e.backtrace.join("\n")
+      if e.is_a?(ActiveRecord::RecordInvalid)
+        logger.error e.record.inspect.to_s
+      end
+      raise e
+    end
   end
-end
