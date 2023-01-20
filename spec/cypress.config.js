4b09dcc23d34fb414e59fcbec1ab0ebbb2c14eb6
Christoph Werner <christoph@chrztoph.com>
2023-01-20 13:53:01 +0100

    Fix linter issues

--- spec/cypress.config.js
+++ spec/cypress.config.js
@@ -1,16 +1,18 @@
-const { defineConfig } = require('cypress')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 10000,
-  video: false,
-  viewportWidth: 1920,
-  viewportHeight: 1080,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-    baseUrl: 'http://localhost:3000',
-  },
-})
+    defaultCommandTimeout: 10000,
+    video: false,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    e2e: {
+        // We've imported your old cypress plugins here.
+        // You may want to clean this up later by importing these.
+        setupNodeEvents(on, config) {
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            return require("./cypress/plugins/index.js")(on, config);
+        },
+        baseUrl: "http://localhost:3000"
+    }
+});
