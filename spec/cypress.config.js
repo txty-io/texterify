3d42ef15ab9729d3c24ced29cbd308051c1a90be
Christoph Werner <christoph@chrztoph.com>
2023-01-20 13:22:59 +0100

    Update cypress

---
+++ spec/cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
+  video: false,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:3000',
+  },
+})
