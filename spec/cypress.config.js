1f92a33ce03e8357709d07a2bf9b6ff13a5af62a
Christoph Werner <christoph@chrztoph.com>
2023-01-20 17:02:31 +0100

    Disable watch for file changes in cypress

--- spec/cypress.config.js
+++ spec/cypress.config.js
@@ -14,5 +14,6 @@ module.exports = defineConfig({
             return require("./cypress/plugins/index.js")(on, config);
         },
         baseUrl: "http://localhost:3000"
-    }
+    },
+    watchForFileChanges: false
 });
