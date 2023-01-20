651ab18b5d747c24845a5f5abe2f5e40f149ffa7
Christoph Werner <christoph@chrztoph.com>
2023-01-20 14:25:54 +0100

    Improve test

--- spec/seeds/seeds_cldr_plurals_spec.rb
+++ spec/seeds/seeds_cldr_plurals_spec.rb
@@ -4,14 +4,27 @@ require 'rails_helper'
 
 RSpec.describe 'Seeds:seeds_cldr_plurals.rb' do
   it 'correctly seeds CLDR plurals' do
+    # Check if data is seeded correctly.
     LanguagePlural.delete_all
-
     expect(LanguagePlural.all).to eq([])
 
     load Rails.root.join('db', 'seeds', 'seeds_cldr_plurals.rb')
-
     language_plurals = LanguagePlural.all.order(code: :ASC).as_json
+    expect(language_plurals).to match_snapshot('language_plurals', { snapshot_serializer: StripSerializer })
 
+    # Check if it updates the data correctly.
+    language_plural = LanguagePlural.find_by(supports_plural_zero: true)
+    language_plural.supports_plural_zero = false
+    language_plural.save!
+
+    load Rails.root.join('db', 'seeds', 'seeds_cldr_plurals.rb')
+    language_plurals = LanguagePlural.all.order(code: :ASC).as_json
+    expect(language_plurals).to match_snapshot('language_plurals', { snapshot_serializer: StripSerializer })
+
+    # Check if running seeds twice is a problem.
+    load Rails.root.join('db', 'seeds', 'seeds_cldr_plurals.rb')
+    load Rails.root.join('db', 'seeds', 'seeds_cldr_plurals.rb')
+    language_plurals = LanguagePlural.all.order(code: :ASC).as_json
     expect(language_plurals).to match_snapshot('language_plurals', { snapshot_serializer: StripSerializer })
   end
 end
