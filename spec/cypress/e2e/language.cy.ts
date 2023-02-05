4622562659be1c9dfaf34db96b9acbc26b505fb8
Christoph Werner <christoph@chrztoph.com>
2023-02-05 16:44:16 +0100

    Fix language cypress test

--- spec/cypress/e2e/language.cy.ts
+++ spec/cypress/e2e/language.cy.ts
@@ -9,20 +9,9 @@ context("language", () => {
         cy.get('[data-id="project-1bfa0215-46d2-478c-8cba-54df0f89c8d4"]').click();
         cy.get('[data-id="project-sidebar-languages"]').click();
         cy.get(".ant-pagination-item-2").click();
-        cy.get('[data-id="project-languages-search"]').type("a");
+        cy.get('[data-id="project-languages-search"]').type("sh");
 
-        const languages_that_should_be_visible = [
-            "a000",
-            "a111",
-            "a222",
-            "a333",
-            "a444",
-            "a555",
-            "a666",
-            "a777",
-            "a888",
-            "a999"
-        ];
+        const languages_that_should_be_visible = ["English", "Spanish"];
 
         languages_that_should_be_visible.forEach((language) => {
             cy.contains(language).should("be.visible");
