7c77f78f074fc373114688498a95b23a8c7876fd
Christoph Werner <>
2023-04-21 22:39:32 +0200

    Fix format

--- config/initializers/license.rb
+++ config/initializers/license.rb
@@ -1,3 +1,7 @@
-public_key = OpenSSL::PKey::RSA.new(File.read('license_key.pub')) if Rails.env.production?
-public_key = OpenSSL::PKey::RSA.new(File.read('test_license_key.pub')) if !Rails.env.production?
+if Rails.env.production?
+  public_key = OpenSSL::PKey::RSA.new(File.read('license_key.pub'))
+else
+  public_key = OpenSSL::PKey::RSA.new(File.read('test_license_key.pub'))
+end
+
 Gitlab::License.encryption_key = public_key
