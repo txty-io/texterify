78ee526bce9ce9e667c8fd7394db542729188ae5
Christoph Werner <>
2023-05-01 18:37:39 +0200

    Refactor organization site to functional component and add limits view

--- app/javascript/components/sites/dashboard/OrganizationSite.tsx
+++ app/javascript/components/sites/dashboard/OrganizationSite.tsx
@@ -1,119 +1,134 @@
-import { Layout, message, Tooltip } from "antd";
-import { observer } from "mobx-react";
+import { Layout, Statistic, Tooltip } from "antd";
 import * as React from "react";
-import { RouteComponentProps } from "react-router";
-import { OrganizationsAPI } from "../../api/v1/OrganizationsAPI";
+import { useParams } from "react-router";
 import { IProject } from "../../api/v1/ProjectsAPI";
 import { NewProjectFormModal } from "../../forms/NewProjectFormModal";
+import { useOrganization } from "../../network/useOrganization";
 import { Routes } from "../../routing/Routes";
-import { dashboardStore } from "../../stores/DashboardStore";
 import { Breadcrumbs } from "../../ui/Breadcrumbs";
 import { OrganizationAvatar } from "../../ui/OrganizationAvatar";
 import { PrimaryButton } from "../../ui/PrimaryButton";
 import { ProjectsList } from "../../ui/ProjectsList";
+import { history } from "../../routing/history";
 
-type IProps = RouteComponentProps<{ organizationId: string }>;
-interface IState {
-    addDialogVisible: boolean;
-    responseOrganization: any;
-}
+export function OrganizationSite() {
+    const { organizationId } = useParams<{ organizationId: string }>();
 
-@observer
-class OrganizationSite extends React.Component<IProps, IState> {
-    state: IState = {
-        addDialogVisible: false,
-        responseOrganization: null
-    };
+    const [addDialogVisible, setAddDialogVisible] = React.useState(false);
 
-    async componentDidMount() {
-        try {
-            const responseOrganization = await OrganizationsAPI.getOrganization(this.props.match.params.organizationId);
+    const { data: organizationData, isLoading: organizationDataLoading } = useOrganization({
+        organizationId: organizationId
+    });
 
-            this.setState({
-                responseOrganization: responseOrganization
-            });
-        } catch (error) {
-            console.error(error);
-            message.error("Failed to load organization.");
-        }
-    }
-
-    render() {
-        return (
-            <>
-                <Layout style={{ padding: "0 24px 24px", margin: "0", width: "100%" }}>
-                    <Breadcrumbs breadcrumbName="organization" />
-                    <Layout.Content style={{ margin: "24px 16px 0", minHeight: 360, maxWidth: 640 }}>
-                        <div style={{ display: "flex", alignItems: "center", marginBottom: 24 }}>
-                            <h1 style={{ display: "flex", alignItems: "center", marginBottom: 0 }}>
-                                <OrganizationAvatar
-                                    organization={dashboardStore.currentOrganization}
-                                    style={{ marginRight: 16 }}
-                                />
-                                <div style={{ marginRight: 40 }}>
-                                    {dashboardStore.currentOrganization &&
-                                        dashboardStore.currentOrganization.attributes.name}
-                                </div>
-                                <Tooltip
-                                    title={
-                                        dashboardStore.currentOrganization?.attributes.project_limit_reached
-                                            ? "Project limit reached. Upgrade to a higher plan to create more projects."
-                                            : undefined
-                                    }
-                                >
-                                    <div>
-                                        <PrimaryButton
-                                            data-id="organization-create-project"
-                                            onClick={() => {
-                                                this.setState({ addDialogVisible: true });
-                                            }}
-                                            disabled={
-                                                dashboardStore.currentOrganization?.attributes.project_limit_reached
-                                            }
-                                        >
-                                            Create project
-                                        </PrimaryButton>
-                                    </div>
-                                </Tooltip>
-                            </h1>
+    return (
+        <>
+            <Layout style={{ padding: "0 24px 24px", margin: "0", width: "100%" }}>
+                <Breadcrumbs breadcrumbName="organization" />
+                <Layout.Content style={{ margin: "24px 16px 0", minHeight: 360, maxWidth: 1040 }}>
+                    <h1 style={{ display: "flex", alignItems: "center", marginBottom: 0 }}>
+                        <OrganizationAvatar organization={organizationData?.data} style={{ marginRight: 16 }} />
+                        <div style={{ marginRight: 40 }}>
+                            {organizationData?.data && organizationData?.data.attributes.name}
                         </div>
-                        <div style={{ display: "flex", marginTop: 40 }}>
-                            <div style={{ width: "100%" }}>
-                                <h3 style={{ marginBottom: 24 }}>Projects</h3>
-                                <ProjectsList
-                                    loading={
-                                        !this.state.responseOrganization ||
-                                        dashboardStore.currentOrganization.attributes.current_user_deactivated
-                                    }
-                                    projects={
-                                        this.state.responseOrganization && this.state.responseOrganization.included
-                                            ? this.state.responseOrganization.included.filter((included) => {
-                                                  return included.type === "project";
-                                              })
-                                            : []
-                                    }
+                        <Tooltip
+                            title={
+                                organizationData?.data?.attributes.project_limit_reached
+                                    ? "Project limit reached. Upgrade to a higher plan to create more projects."
+                                    : undefined
+                            }
+                        >
+                            <div>
+                                <PrimaryButton
+                                    data-id="organization-create-project"
+                                    onClick={() => {
+                                        setAddDialogVisible(true);
+                                    }}
+                                    disabled={organizationData?.data?.attributes.project_limit_reached}
+                                >
+                                    Create project
+                                </PrimaryButton>
+                            </div>
+                        </Tooltip>
+                    </h1>
+                    <div style={{ display: "flex", gap: 40, marginTop: 40 }}>
+                        <div style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
+                            <h3 style={{ marginBottom: 24 }}>Projects</h3>
+                            <ProjectsList
+                                loading={!!organizationData?.data.attributes.current_user_deactivated}
+                                projects={(organizationData?.included || []).filter((included) => {
+                                    return included.type === "project";
+                                })}
+                            />
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "column", flexGrow: 1, maxWidth: 400 }}>
+                            <h3>Limits</h3>
+                            <div style={{ display: "flex", gap: 56, marginTop: 24 }}>
+                                <Statistic
+                                    title="Users"
+                                    value={`${organizationData?.data.attributes.active_users_count}/${
+                                        organizationData?.data.attributes.user_limit === null
+                                            ? "∞"
+                                            : organizationData?.data.attributes.user_limit
+                                    }`}
+                                    loading={organizationDataLoading}
+                                />
+                                <Statistic
+                                    title="Projects"
+                                    value={`${organizationData?.data.attributes.project_count}/${
+                                        organizationData?.data.attributes.project_limit === null
+                                            ? "∞"
+                                            : organizationData?.data.attributes.project_limit
+                                    }`}
+                                    loading={organizationDataLoading}
+                                />
+                                <Statistic
+                                    title="Keys"
+                                    value={`${organizationData?.data.attributes.key_count}/${
+                                        organizationData?.data.attributes.key_limit === null
+                                            ? "∞"
+                                            : organizationData?.data.attributes.key_limit
+                                    }`}
+                                    loading={organizationDataLoading}
                                 />
                             </div>
+                            <Statistic
+                                title="Max languages per project"
+                                value={
+                                    organizationData?.data.attributes.language_limit_per_project === null
+                                        ? "∞"
+                                        : organizationData?.data.attributes.language_limit_per_project
+                                }
+                                loading={organizationDataLoading}
+                                style={{ marginTop: 40 }}
+                            />
+                            <Statistic
+                                title="Machine translation characters"
+                                value={`${organizationData?.data.attributes.machine_translation_character_usage}/${
+                                    organizationData?.data.attributes.machine_translation_character_limit === null
+                                        ? "∞"
+                                        : organizationData?.data.attributes.machine_translation_character_limit
+                                }`}
+                                loading={organizationDataLoading}
+                                style={{ marginTop: 40 }}
+                            />
                         </div>
-                    </Layout.Content>
-                </Layout>
+                    </div>
+                </Layout.Content>
+            </Layout>
 
-                <NewProjectFormModal
-                    organization={dashboardStore.currentOrganization}
-                    visible={this.state.addDialogVisible}
-                    onCancelRequest={() => {
-                        this.setState({ addDialogVisible: false });
-                    }}
-                    newProjectFormProps={{
-                        organizationId: this.props.match.params.organizationId,
-                        onChanged: (project: IProject) => {
-                            this.props.history.push(Routes.DASHBOARD.PROJECT.replace(":projectId", project.id));
-                        }
-                    }}
-                />
-            </>
-        );
-    }
+            <NewProjectFormModal
+                organization={organizationData?.data}
+                visible={addDialogVisible}
+                onCancelRequest={() => {
+                    setAddDialogVisible(false);
+                }}
+                newProjectFormProps={{
+                    organizationId: organizationId,
+                    onChanged: (project: IProject) => {
+                        history.push(Routes.DASHBOARD.PROJECT.replace(":projectId", project.id));
+                    }
+                }}
+            />
+        </>
+    );
 }
-
-export { OrganizationSite };
