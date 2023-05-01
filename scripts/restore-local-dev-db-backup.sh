f5f8b8ad89541d2542cf8360d91d015b4866397f
Christoph Werner <>
2023-05-01 18:38:11 +0200

    Fix contained id for backup import script

--- scripts/restore-local-dev-db-backup.sh
+++ scripts/restore-local-dev-db-backup.sh
@@ -1,5 +1,5 @@
 set -e
-db_container_id=$(docker ps -aqf "name= texterify-db-1")
+db_container_id=$(docker ps -aqf "name= txty-db-1")
 docker exec -i $db_container_id psql -U postgres -c "drop database texterify_development"
 docker exec -i $db_container_id psql -U postgres -c "create database texterify_development"
 cat $1 | docker exec -i $db_container_id psql -U postgres -d texterify_development
