aca2351d18294c7a211c06ea7b0be80c5da9e62f
Christoph Werner <>
2023-08-10 17:15:39 +0200

    Fix as const

--- app/models/export_config.rb
+++ app/models/export_config.rb
@@ -405,9 +405,9 @@ class ExportConfig < ApplicationRecord
 
     language_file = Tempfile.new(language.id.to_s)
     language_file.print("const #{language.name.downcase} = ")
-    language_file.puts("#{JSON.pretty_generate(plural_data)};")
+    language_file.puts("#{JSON.pretty_generate(plural_data)} as const;")
     language_file.puts
-    language_file.puts("export { #{language.name.downcase} } as const;")
+    language_file.puts("export { #{language.name.downcase} };")
     language_file.close
 
     [{ path: self.filled_file_path(language), file: language_file }]
