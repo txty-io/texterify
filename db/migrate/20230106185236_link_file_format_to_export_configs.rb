ccde1d6dc0b0b8019ebbf4eb9438a93ff08ee90e
Christoph Werner <>
2023-08-31 10:42:22 +0200

    Fall back to JSON if format could not be found in migration

--- db/migrate/20230106185236_link_file_format_to_export_configs.rb
+++ db/migrate/20230106185236_link_file_format_to_export_configs.rb
@@ -7,12 +7,12 @@ class LinkFileFormatToExportConfigs < ActiveRecord::Migration[6.1]
     load "#{Rails.root}/db/seeds/seeds_file_formats.rb"
 
     ExportConfig.all.each do |export_config|
-      file_format = FileFormat.find_by!(format: export_config[:file_format])
+      file_format = FileFormat.find_by(format: export_config[:file_format]) || FileFormat.find_by(format: 'json')
       export_config.file_format_id = file_format.id
       export_config.save!
     end
 
-    change_column :export_configs, :file_format_id, :uuid
+    change_column :export_configs, :file_format_id, :uuid, null: false
     remove_column :export_configs, :file_format
   end
 end
