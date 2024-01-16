7d3c43b749af70a79afb729ba0f618451ca3033d
Christoph Werner <hello@chrztoph.com>
2024-01-16 10:43:38 +0100

    Setup tracing

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,6 @@
 if ENV['SENTRY_DSN_BACKEND'].present? && Rails.env.production?
-  Sentry.init { |config| config.dsn = ENV['SENTRY_DSN_BACKEND'] }
+  Sentry.init do |config|
+    config.dsn = ENV['SENTRY_DSN_BACKEND']
+    config.traces_sample_rate = 1.0
+  end
 end
