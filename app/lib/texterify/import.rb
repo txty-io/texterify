89f1baa0b10c8d1a4c042740fcc1c95927a5fa83
Christoph Werner <christoph@voidly.io>
2025-12-04 23:14:42 +0100

    fix: fix issue when importing PO files with BOM

--- app/lib/texterify/import.rb
+++ app/lib/texterify/import.rb
@@ -200,7 +200,10 @@ module Texterify
     end
 
     def self.po?(content)
-      parsed = PoParser.parse(content).to_h
+      parsed =
+        PoParser.parse(
+          content.gsub!("\xEF\xBB\xBF".force_encoding('UTF-8'), '') # Remove BOM if present
+        ).to_h
       json = {}
 
       parsed.each do |entry|
